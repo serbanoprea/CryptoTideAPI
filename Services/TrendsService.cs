bba6fc0fdb2102d9327c2cd44d64efd2cb98ba62
Serban Oprea <serban.oprea@yahoo.com>
2020-02-09 22:52:30 +0000

    Updates service

--- Services/TrendsService.cs
+++ Services/TrendsService.cs
@@ -13,17 +13,22 @@ namespace Services
     public class TrendsService : ITrendsService
     {
         private readonly CryptoTideDBContext Context;
+        private readonly int LastHour;
+
         public TrendsService(IConfiguration config)
         {
             Context = new CryptoTideDBContext(config);
+
+            // TODO(Serban): Hacky, will need to keep value in memory and be updated by the data pipeline
+            LastHour = Context.HourlyTrends.Where(t => t.Date == DateTime.Now.Date).Max(t => t.Hour);
         }
 
         public async Task<IEnumerable<HourlyTrend>> GetTrends(DateTime from, DateTime to, int limit=20)
         {
             var trends = await Context
                 .HourlyTrends
-                .Where(t => t.Date > from && t.Date < to)
-                .OrderBy(t => t.PercentageIncrease / t.ConsecutiveIncreases)
+                .Where(t => t.Date == DateTime.Now.Date && t.Hour == LastHour)
+                .OrderBy(t => t.PercentageIncrease * t.ConsecutiveIncreases)
                 .Take(limit)
                 .ToListAsync();
 
@@ -32,7 +37,11 @@ namespace Services
 
         public async Task<IEnumerable<HourlyTrend>> GetTrendBySymbol(string symbol)
         {
-            var trends = await Context.HourlyTrends.Where(t => t.Symbol.Equals(symbol, StringComparison.InvariantCultureIgnoreCase)).ToListAsync();
+            var trends = await Context
+                .HourlyTrends
+                .Where(t => t.Symbol.Equals(symbol, StringComparison.InvariantCultureIgnoreCase))
+                .ToListAsync();
+
             return trends;
         }
     }
